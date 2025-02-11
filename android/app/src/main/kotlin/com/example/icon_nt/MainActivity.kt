f5449bb9b3f84b45b5de9289cc9bad74ef9b61b7
newuser <ilhomjonibragimov1502@gmail.com>
2025-02-11 09:11:29 +0500

    Icon and IconButton

---
+++ android/app/src/main/kotlin/com/example/icon_nt/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.icon_nt
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Icons")),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Center(
+            child: Row(
+              children: [
+                //icon
+                Icon(
+                  Icons.edit,
+                size: 100,
+                color: Colors.red,
+              ),
+              //iconButton
+              IconButton(onPressed: (){
+                print("Icon bosildi");
+              }, icon: Icon(Icons.delete, size: 100,))
+              ]
+            )
+      ),
+      ),
+    );
+  }
+}
